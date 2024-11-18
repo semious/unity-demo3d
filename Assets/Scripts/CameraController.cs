2d35d8f299e428faef0ebbc3cc6d9467ff9abdbf
semious <semious@gmail.com>
2024-11-18 09:43:39 +0800

    fixed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,26 +4,6 @@ public class CameraController : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        offset = transform.position - player.transform.position;
-    }
-
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        transform.position = player.transform.position + offset;
-    }
-}
-
-using UnityEngine;
-
-public class CameraController : MonoBehaviour
-{
-    public GameObject player;
-    private Vector3 offset;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
